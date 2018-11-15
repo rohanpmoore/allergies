b5a867800d9085bf3b839535cbf37d8562d7bbd8
Kristin Brewer_Lowe and Rohan Moore <rohan@>
2018-11-15 09:42:13 -0800

    finalize project

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -1,2 +1,15 @@
 def allergies(number)
+  allergies = {"eggs" => 1, "peanuts" => 2, "shellfish" => 4, "strawberries" => 8, "tomatoes" => 16, "chocolate" => 32, "pollen" => 64, "cats" => 128}
+  patient_allergies = []
+  allergy_number = number
+  exponent = 7
+  until exponent < 0
+    power = 2**exponent
+    if allergy_number >= power
+      patient_allergies.unshift(allergies.key(power))
+      allergy_number -= power
+    end
+    exponent -= 1
+  end
+  patient_allergies
 end
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -6,4 +6,10 @@ describe('allergies') do
   it('will return the egg allergy for the number 1') do
     expect(allergies(1)).to(eq(["eggs"]))
   end
+  it('will return eggs and peanuts for the number 3') do
+    expect(allergies(3)).to(eq(["eggs", "peanuts"]))
+  end
+  it('will return an array of allergies corresponding to the allergy score') do
+    expect(allergies(152)).to(eq(["strawberries","tomatoes","cats"]))
+  end
 end
